cb48bb9a14c36826bc110057722ad3d8db72a5e3
Rensi Arteaga Copari <rensi4rn@gmail.com>
2015-08-06 10:19:51 -0400

    arreglo bug de rendiciones

--- reportes/RCbteRendicion.php
+++ reportes/RCbteRendicion.php
@@ -29,7 +29,7 @@ class RCbteRendicion extends  ReportePDF {
 	
 	function Header() {
 		//cabecera del reporte
-		$this->Image(dirname(__FILE__).'/../../lib/imagenes/logos/logo.png', 10,5,45,20);
+		//$this->Image(dirname(__FILE__).'/../../lib/imagenes/logos/logo.png', 10,5,45,20);
 		$this->ln(5);
 		$this->SetFont('','BU',12);
 		
